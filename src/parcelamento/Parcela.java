5fbeaa7a5e47045d182ea46b53c13f18877724e9
Herick Exterkoetter <herickherick47@gmail.com>
2020-09-18 16:10:47 -0300

    :fire: feat: initial parcelamento

---
+++ src/parcelamento/Parcela.java
@@ -0,0 +1,9 @@
+package parcelamento;
+
+import java.time.LocalDate;
+
+public class Parcela {
+
+    private LocalDate data;
+    private float valor;
+}
---
+++ src/parcelamento/Venda.java
@@ -0,0 +1,12 @@
+package parcelamento;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Venda {
+
+    private String numero;
+    private LocalDate data;
+    private float valor;
+    private List<Parcela> parcelas;
+}
